3db54908740c107237cd1a8b2f60c860c578c42a
newgate21 <124586622+newgate21@users.noreply.github.com>
2024-04-05 11:28:02 +0200

    removed debug logs in WavesController

--- Assets/Scripts/Common/WavesController.cs
+++ Assets/Scripts/Common/WavesController.cs
@@ -6,23 +6,16 @@ public class WavesController : MonoBehaviour
 {
     [SerializeField] private string filenameCsv;
     [SerializeField] private string filenameJson;
-    private List<Dictionary<string, object>> wavesData = new();
+    private List<Dictionary<string, object>> wavesDataCsv = new();
     private JSONWaves wavesDataJson = new();
 
     // Start is called before the first frame update
     void Start()
     {
         // CSV version
-        wavesData = CSVParser.Read(filenameCsv);
-
-        for (int i = 0; i < wavesData.Count; i++)
-        {
-            Debug.Log("Wave: " + wavesData[i]["wave"] + " Enemy1 count: " + wavesData[i]["enemy1"] + " Enemy2 count: " + wavesData[i]["enemy2"]);
-        }
+        wavesDataCsv = CSVParser.Read(filenameCsv);
 
         //JSON version
         wavesDataJson = JSONWaves.CreateFromJSON(TextFileReader.ReadFileAsText(filenameJson));
-
-        Debug.Log("Wave: " + wavesDataJson.waves[0].id + " Enemy1 id: " + wavesDataJson.waves[0].enemies[0].id + " Enemy1 spawnTime: " + wavesDataJson.waves[0].enemies[0].spawnTime);
     }
 }
